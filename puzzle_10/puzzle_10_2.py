920f593fd510ba00cab3bbeaf17079bc0a895629
Jakub Foltyn <foltyn.kuba@gmail.com>
2019-12-22 10:52:24 +0100

    Solved puzzle 10.2

--- puzzle_10/puzzle_10_2.py
+++ puzzle_10/puzzle_10_2.py
@@ -4,9 +4,13 @@ import sys
 
 # raw_input = open(os.path.join(sys.path[0], 'input_test_1_2_35')).read()
 # raw_input = open(os.path.join(sys.path[0], 'input_test_5_8_33')).read()
-# raw_input = open(os.path.join(sys.path[0], 'input_test_11_13_210')).read()
+# raw_input = open(os.path.join(sys.path[0], 'input_test_3_4')).read()
+# raw_input = open(os.path.join(sys.path[0], 'input_test_8_3')).read()
 raw_input = open(os.path.join(sys.path[0], 'input')).read()
+station_location = (26, 28)
 
+# raw_input = open(os.path.join(sys.path[0], 'input_test_11_13_210')).read()
+# station_location = (11,13)
 
 map = []
 
@@ -14,19 +18,18 @@ for map_row in raw_input.split('\n'):
     map.append(list(map_row))
 
 
-asteroid_locations = {}
+asteroid_locations = set()
 
 
 for y, map_row in enumerate(map):
     for x, item in enumerate(map_row):
-        if item == '#':
-            asteroid_locations[(x, y)] = set()
+        if item != '.':
+            asteroid_locations.add((x, y))
 
 
-station_location = (26, 28)
 
 asteroid_by_direction = {}
-for asteroid in asteroid_locations.keys():
+for asteroid in asteroid_locations:
     if asteroid == station_location:
         continue
 
@@ -44,33 +47,42 @@ for asteroid in asteroid_locations.keys():
 
 def degree_from_y_axis(vector):
     x = vector[0]
-    y = vector[1]
+    y = -vector[1]
 
     propper_rads = math.atan2(y, x)
-    if x < 0 and y >= 0:
-        # 2 quadrant
-        return propper_rads - (math.pi * (1/2))
-    elif x <= 0 and y < 0:
-        # 3 quadrant
-        return propper_rads + (math.pi * (3/2))
-    elif x >= 0 and y < 0:
-        # 4 quadrant
-        return propper_rads + (math.pi * (3/2))
-    elif x >= 0 and y >= 0:
+    if x >= 0 and y >= 0:
         # 1 quadrant
         return propper_rads + (math.pi * (3/2))
+    if x >= 0 and y < 0:
+        # 4 quadrant
+        return propper_rads + (math.pi * (3/2))
+    if x <= 0 and y <= 0:
+        # 3 quadrant
+        return propper_rads + (math.pi * (3/2))
+    if x < 0 and y > 0:
+        # 2 quadrant
+        return propper_rads - (math.pi * (1/2))
 
 
 asteroid_by_direction_sorted = sorted(asteroid_by_direction.items(), key= lambda asteroid: -degree_from_y_axis(asteroid[0]) ) 
-
+asteroids_ordered_by_distance = list()
 for direction, asteroids in asteroid_by_direction_sorted:
-    asteroids = sorted(asteroids, key=lambda pos: ((pos[0] - 26) ** 2) + ((pos[1] - 28) ** 2) )
-    if direction == (1, -1):
-        print(123)
+    asteroids_ordered_by_distance.append( sorted(asteroids, key=lambda pos: ((pos[0] - station_location[0]) ** 2) + ((pos[1] - station_location[1]) ** 2) ) )
 
 counter = 0
-for direction, asteroids in asteroid_by_direction_sorted:
-    counter += 1
-    destroyed = asteroids.pop(0)
-    if counter == 200:
-        print(destroyed)
+
+direction_index = 0
+destructed = {}
+while True:
+    asteroids_in_direction = asteroids_ordered_by_distance[direction_index]
+    if len(asteroids_in_direction) != 0:
+        counter += 1
+        asteroid = asteroids_in_direction.pop(0)
+        destructed[counter] = asteroid
+        if counter == 200:
+            print(asteroid)
+            break
+    direction_index = (direction_index + 1) % len(asteroid_by_direction_sorted)
+
+print(destructed)
+
